ad62ceb98f0720e3d76502e7c77ef5408d1cdd8e
rogovskaia <rogovskaia.design@gmail.com>
2025-12-02 20:30:44 +0100

    update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const city = 'Minsk'
 const hobby = 'triathlon'
 
 console.log('Hello', firstName)
+console.log('Hello', lastName)
 console.log('Age:', age)
 console.log('City:', city)
 console.log('Hobby:', hobby)
