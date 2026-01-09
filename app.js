3e06a951d48d23010ad1a8b1844967ee867522b1
rogovskaia <rogovskaia.design@gmail.com>
2026-01-09 13:04:19 +0100

    update app.js

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ console.log('Hello', lastName)
 console.log('Age:', age)
 console.log('City:', city)
 console.log('Hobby:', hobby)
+
+const titleProject = "Оценка стоимости";
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями";
+const screenPrice = 150;
+const percentage = 10;
+const fullPrice = 2000;
+const responsive = true;
